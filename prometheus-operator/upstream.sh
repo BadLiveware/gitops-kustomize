2dc0d4d5a78cfa17e8c276763031d473545a78f9
Fredrik Larsson <you@example.com>
2021-03-09 11:19:08 +0100

    update

--- prometheus-operator/upstream.sh
+++ prometheus-operator/upstream.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 helm dependency update upstream
 helm template \
+    --include-crds \
     --namespace prometheus-operator \
     prometheus-operator \
     ./upstream > resources/upstream.yaml
